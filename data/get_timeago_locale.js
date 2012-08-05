69d67b0ca24ea29203059e947a7a5ba3835eb8d1
Leberwurscht <leberwurscht@hoegners.de>
2012-08-05 15:41:38 +0200

    terminate get_timeago_locale after work is done

--- data/get_timeago_locale.js
+++ data/get_timeago_locale.js
@@ -1,3 +1,8 @@
-self.port.emit("set-data", {
-  "timeago_locale": jQuery.timeago.settings.strings
+self.port.on("start", function() {
+  // workaround to get the jquery.timeago.js locale (it is not possible to include other js files from a content script)
+  self.port.emit("set-data", {
+    "timeago_locale": jQuery.timeago.settings.strings
+  });
+
+  self.port.emit("terminate");
 });
